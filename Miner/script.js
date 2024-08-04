e3b8ebac3001d41953c29f053275959078b1857c
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-08-05 01:16:57 +0500

    sound added

--- Miner/script.js
+++ Miner/script.js
@@ -1,14 +1,42 @@
 const field = document.getElementById("field");
 
-// for(i = 0; i <= 25; i++){
-//     box = document.createElement("div");
-//     box.classList.add("box");
-//     box.classList.add("active");
+for(i = 0; i <= 25; i++){
+    let box = document.createElement("div");
+    box.classList.add("box");
+    box.classList.add("active");
 
-//     field.appendChild(box);
-// }
+    box.isOpened = false;
+
+    if(Math.random() > 0.5){
+        box.isBomb = true;
+    } else {
+        box.isBomb = false;
+    }
+    box.addEventListener("click", () => playAnimation(box));
+    field.appendChild(box);
+}
 
 
-function playAnimation(){
+function playAnimation(box){
+    var audio = new Audio("sounds/click.mp3");
+    audio.play();
 
+    if(box.isOpened) return;
+    box.isOpened = true;
+    img = document.createElement("img");
+    if(box.isBomb) {
+        img.src = "images/bomb.png";
+        box.style.background = "#FF5B61";
+
+    }
+    else {
+        img.src = "images/gem.png";
+        box.style.background = "lightgreen";
+    }
+
+    box.appendChild(img);
+
+    box.classList.remove("active");
+    box.classList.add("open");
+    
 }
