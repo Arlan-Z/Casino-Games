c3d8dde1b200e2fb965f2114a57aed58d932dbb8
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-08-07 02:55:46 +0500

    first working ver

--- Miner/script.js
+++ Miner/script.js
@@ -1,18 +1,27 @@
 const field = document.getElementById("field");
+const mines = document.getElementById("mines");
 
-for(i = 0; i <= 25; i++){
+const profitField = document.getElementById("profit");
+const betField = document.getElementById("bet");
+
+const moneyText = document.getElementById("money");
+
+const betBtn = document.getElementById("bet-btn");
+
+const afterElements = document.querySelectorAll(".after")
+const beforeElements = document.querySelectorAll(".before")
+
+var money = 0;
+var isLose = false;
+
+var remainingBoxes = 25;
+var cash = 0;
+
+changeMoney(0);
+for(i = 0; i < 25; i++){
     let box = document.createElement("div");
     box.classList.add("box");
-    box.classList.add("active");
-
-    box.isOpened = false;
-
-    if(Math.random() > 0.5){
-        box.isBomb = true;
-    } else {
-        box.isBomb = false;
-    }
-    box.addEventListener("click", () => playAnimation(box));
+    box.classList.add("disabled");
     field.appendChild(box);
 }
 
@@ -29,16 +38,115 @@ function playAnimation(box){
     if(box.isBomb) {
         img.src = "images/bomb.png";
         box.style.background = "#FF5B61";
-
+        isLose = true;
     }
     else {
         img.src = "images/gem.png";
         box.style.background = "lightgreen";
+        increaseCash();
     }
 
     box.appendChild(img);
 
     box.classList.remove("active");
     box.classList.add("open");
+    if(isLose) gameEnd(); 
     
+}
+
+function bet(){
+    if(betField.value > money) return;
+    changeMoney(betField.value, -1);
+    remainingBoxes = 25;
+    const boxes = field.children;
+    var minesCount = mines.value;
+    var boxCnt = 25;
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].classList.add("active");
+        boxes[i].classList.remove("disabled");
+        boxes[i].innerHTML = "";
+        boxes[i].style.background = "rgb(71, 71, 101)";
+        boxes[i].isOpened = false;
+
+        if(Math.random() < (minesCount / boxCnt) && minesCount > 0){
+            boxes[i].isBomb = true;
+            minesCount--;
+        } else {
+            boxes[i].isBomb = false;
+            boxCnt--;
+        }
+
+        boxes[i].addEventListener("click", () => playAnimation(boxes[i]));
+    }
+    betBtn.style.display = "none";
+    afterElements.forEach((el) => {
+        el.style.display = "block";
+    });
+
+    beforeElements.forEach((el) => {
+        el.disabled = true;
+    });
+}
+
+function gameEnd(){
+    isLose = false;
+    const boxes = field.children;
+    profitField.value = 0;
+    cash = 0;
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].classList.add("disabled");
+        boxes[i].classList.remove("active");
+        boxes[i].classList.remove("open");
+
+        if(!boxes[i].isOpened) {
+            img = document.createElement("img");
+            boxes[i].appendChild(img);
+            if(boxes[i].isBomb) {
+                img.src = "images/bomb.png";
+            }
+            else {
+                img.src = "images/gem.png";
+            }
+            boxes[i].firstChild.style.transform = "scale(0.75)";
+        }
+        boxes[i].isOpened = true;
+    }
+
+    betBtn.style.display = "block";
+
+    afterElements.forEach((el) => {
+        el.style.display = "none";
+    });
+
+    beforeElements.forEach((el) => {
+        el.disabled = false;
+    });
+}
+
+function changeMoney(value, sub = 1){
+    value = value * sub;
+    money = money += value;
+    moneyText.innerHTML = money.toFixed(4);
+    console.log(money);
+}
+
+function increaseCash(){
+    cash = betField.value * (1 + (mines.value) / (remainingBoxes + 1 - mines.value));
+    remainingBoxes--;
+    profitField.value = cash.toFixed(4);
+}
+
+function cashout(){
+    changeMoney(cash);
+    gameEnd();
+}
+
+function addMoney(){
+    let amount = prompt("Enter Amount:");
+    if (amount == null || amount == "") {
+        console.log("operation cancelled");
+      } else {
+        console.log(amount + "$ added");
+        changeMoney(amount);
+      }
 }
